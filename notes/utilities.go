d5c3678a6175800dc8651ae9b4953352058aafe5
Tuomas <oituomas@gmail.com>
2024-01-26 21:39:47 +0200

    fixed logic error

--- notes/utilities.go
+++ notes/utilities.go
@@ -17,12 +17,13 @@ func (implStrings) GetStringFromCmd() string {
 	return input
 }
 
+// returns -1 if userinput is not int
 func (implStrings) GetIntFromCmd() int {
 	var input string
 	fmt.Scanln(&input)
 	input = strings.Trim(input, " ")
 	userinput, err := strconv.Atoi(input)
-	if err == nil {
+	if err != nil {
 		return -1
 	}
 	return userinput
